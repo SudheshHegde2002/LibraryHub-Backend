d5029a0a408b452b9f0ebb5b5c11cfa2f22d327d
Sudhesh Hegde <hegdesud2002@gmail.com>
2025-12-02 11:34:51 +0530

    Wrote AuthorsService with CRUD operations using Prisma for managing authors.

--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class AuthorsService {}
+export class AuthorsService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data: { name: string }) {
+    return this.prisma.authors.create({ data });
+  }
+
+  findAll() {
+    return this.prisma.authors.findMany();
+  }
+
+  update(id: number, data: { name?: string }) {
+    return this.prisma.authors.update({
+      where: { id },
+      data,
+    });
+  }
+
+  delete(id: number) {
+    return this.prisma.authors.delete({
+      where: { id },
+    });
+  }
+}
