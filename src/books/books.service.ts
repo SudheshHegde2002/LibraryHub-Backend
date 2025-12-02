a8914a24729b04beb11737b29ce6288b53081e64
Sudhesh Hegde <hegdesud2002@gmail.com>
2025-12-02 10:55:54 +0530

    Implemented BooksService with CRUD  using prisma

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data: { title: string; author_id: number }) {
+    return this.prisma.books.create({
+      data,
+    });
+  }
+
+  findAll(filters: { author_id?: number; is_borrowed?: boolean }) {
+    return this.prisma.books.findMany({
+      where: {
+        author_id: filters.author_id,
+        is_borrowed: filters.is_borrowed,
+      },
+      include: { Authors: true },
+    });
+  }
+
+  update(id: number, data: { title?: string; author_id?: number }) {
+    return this.prisma.books.update({
+      where: { id },
+      data,
+    });
+  }
+
+  delete(id: number) {
+    return this.prisma.books.delete({
+      where: { id },
+    });
+  }
+}
