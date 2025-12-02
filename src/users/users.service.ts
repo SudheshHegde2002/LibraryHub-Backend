abed56f468f5a2b59e47258248a87f3fe0df02f7
Sudhesh Hegde <hegdesud2002@gmail.com>
2025-12-02 12:28:28 +0530

    Wrote UsersService for user mgmt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data: { name: string; email: string }) {
+    return this.prisma.users.create({ data });
+  }
+
+  findAll() {
+    return this.prisma.users.findMany();
+  }
+}
