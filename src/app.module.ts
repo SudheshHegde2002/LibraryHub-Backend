9a84ff051c8eaac6dfb9a788f9258552d4e52a79
Sudhesh Hegde <hegdesud2002@gmail.com>
2025-12-02 15:26:50 +0530

    Added BorrowModule with controller and service and update AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,11 @@ import { ConfigModule } from '@nestjs/config';
 import { BooksModule } from './books/books.module';
 import { AuthorsModule } from './authors/authors.module';
 import { UsersModule } from './users/users.module';
+import { BorrowModule } from './borrow/borrow.module';
 
 
 @Module({
-  imports: [ ConfigModule.forRoot({ isGlobal: true }), AuthModule, BooksModule, AuthorsModule, UsersModule],
+  imports: [ ConfigModule.forRoot({ isGlobal: true }), AuthModule, BooksModule, AuthorsModule, UsersModule, BorrowModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
